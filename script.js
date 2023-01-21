36a401c4971a2b88fca518e656b48ab8c0fe6aae
carson-coder <carsondpool@gmail.com>
2023-01-20 21:43:02 -0500

    Fixed Design For Mobile

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function popup(text) {
   document.getElementById("popup").firstChild.innerHTML = text
-  document.getElementById("popup").className = "popup"
+  document.getElementById("popup").className = "show popup"
   timer = setTimeout(() => {
-    document.getElementById("popup").className = "transparent popup"
+    document.getElementById("popup").className = "popup"
   }, 2000)
 }
+
+setTimeout(() => {
+  document.getElementById("popup").className = "popup"
+}, 500)
